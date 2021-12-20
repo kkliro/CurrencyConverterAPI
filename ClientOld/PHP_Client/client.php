d55b856ebb3b990f33a88903067e08939a0411d1
konstantinklir <konstantin-klir@outlook.com>
2021-12-20 17:37:24 -0500

    Removed old client

--- ClientOld/PHP_Client/client.php
+++
@@ -1,6 +0,0 @@
-<?php
-    // A- Reading data as a stream
-    $fp = fopen("http://localhost/videoconversionservice/api/client/all", "r");
-    $data = stream_get_contents($fp, -1, 0);
-    echo $data;
-?>
