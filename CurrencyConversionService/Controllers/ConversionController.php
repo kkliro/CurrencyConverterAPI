6f2dadf99374ff08ad76888cb7c1a061005c18bc
konstantinklir <konstantin-klir@outlook.com>
2021-12-19 23:43:59 -0500

    Log insertions for conversion controller

--- CurrencyConversionService/Controllers/ConversionController.php
+++ CurrencyConversionService/Controllers/ConversionController.php
@@ -1,6 +1,7 @@
 <?php
 require_once("../Models/Conversion.php");
 require_once("../Models/Client.php");
+require_once("../Logs/LogHandler.php");
 
 class ConversionController
 {
@@ -35,27 +36,26 @@ class ConversionController
     }
 
     function convert($originalCurrency, $convertedCurrency, $originalAmount){
-        // CAD -> USD; (USD_Rate / CAD_Rate) * amount_specified
-        // set API Endpoint and API key
         $endpoint = 'latest';
         $access_key = '22206d1ad17012cbe8f4c2e9a9f0f4b6';
 
-        // Initialize CURL:
         $ch = curl_init('http://api.exchangeratesapi.io/v1/'.$endpoint.'?access_key='.$access_key.'');
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-        // Store the data:
         $json = curl_exec($ch);
         curl_close($ch);
 
-        // Decode JSON response:
         $exchangeRates = json_decode($json, true);
 
-        // Access the exchange rate values, e.g. GBP:
+        LogHandler::write("Updated real-time exchanges rates...", "INFO");
+
+        LogHandler::write("Converted {$originalCurrency} -> {$convertedCurrency}", "INFO");
+
         $originalCurrency = $exchangeRates['rates'][$originalCurrency];
         $convertedCurrency = $exchangeRates['rates'][$convertedCurrency];
 
     	$convert = ($convertedCurrency / $originalCurrency) * $originalAmount;
+
     	return $convert = round($convert, 2);
     }
 
